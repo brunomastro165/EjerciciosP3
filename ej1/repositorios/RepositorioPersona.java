6e798e8faf4c06dcbdfe8702c73471362fd36a1e
Bruno Mastropietro <127962081+brunomastro165@users.noreply.github.com>
2023-08-29 17:56:40 -0300

    Ejercicio 1

---
+++ ej1/repositorios/RepositorioPersona.java
@@ -0,0 +1,9 @@
+package ej1.repositorios;
+
+import ej1.entidades.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepositorioPersona extends JpaRepository<Persona, Long> {
+}
