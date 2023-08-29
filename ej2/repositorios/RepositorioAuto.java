e1ca6614fb2f96b95d53d048a8b3c65d4f1dfd7f
Bruno Mastropietro <127962081+brunomastro165@users.noreply.github.com>
2023-08-29 19:32:52 -0300

    Delete ej2 directory

--- ej2/repositorios/RepositorioAuto.java
+++
@@ -1,9 +0,0 @@
-package com.example.ej2.repositorios;
-
-import com.example.ej2.entidades.Auto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositorioAuto extends JpaRepository<Auto, Long> {
-}
--- ej2/repositorios/RepositorioPersonaEj2.java
+++
@@ -1,9 +0,0 @@
-package com.example.ej2.repositorios;
-
-import com.example.ej2.entidades.PersonaEj2;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositorioPersonaEj2 extends JpaRepository<PersonaEj2, Long> {
-}
